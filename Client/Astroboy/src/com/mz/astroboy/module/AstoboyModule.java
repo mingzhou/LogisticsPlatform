e09e20b31f3c313f49120ffa1d55704586e8fb25
mingzhou <mingzhou.zhuang@gmail.com>
2013-12-26 13:51:28 +0800

    完成了APP客户端的框架设计 v1.0 修改了目录结构
    Signed-off-by: mingzhou <mingzhou.zhuang@gmail.com>

---
+++ Client/Astroboy/src/com/mz/astroboy/module/AstoboyModule.java
@@ -0,0 +1,26 @@
+package com.mz.astroboy.module;
+
+import com.google.inject.AbstractModule;
+import com.mz.astroboy.entity.IAstroboy;
+import com.mz.astroboy.entity.internal.Astroboy;
+import com.mz.astroboy.service.IAstroboyRemoteControl;
+import com.mz.astroboy.service.internal.AstroboyRemoteControl;
+
+/**
+ * 模块化安装各个组件
+ * 实现了接口和服务的分离
+ * @author Mingzhou Zhuang (mingzhou.zhuang@gmail.com)
+ *
+ */
+public class AstoboyModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		//	指定要使用的interface是由哪个具体的service实现的
+		bind(IAstroboy.class).to(Astroboy.class);
+		bind(IAstroboyRemoteControl.class).to(AstroboyRemoteControl.class);
+		
+		//	如果直接使用具体的service，当然就不需要再绑定了，比如这里的com.mz.astroboy.entity.internal.ContextInfo
+	}
+
+}
