edc600ad3e35546b0d174ff2ddfa0af616b5868d
Brilliant von Shakespeare <brilliantshakespeare@gmail.com>
2014-04-05 18:39:37 +0800

    finish 56QQ

--- Server/crawlers/crawler_runner.py
+++ Server/crawlers/crawler_runner.py
@@ -1,11 +1,12 @@
 from mongo_writer import MongoWriter
 from cwt_crawler import ChinawutongCrawler
-#from qq56_crawler import QQ56GoodCrawler
+from qq56_crawler import QQ56Crawler
 
 db = MongoWriter()
 cs = []
 cs.append(ChinawutongCrawler())
-#cs.append(QQ56GoodCrawler())
+cs.append(QQ56Crawler())
+
 for c in cs:
     c.set_db(db)
     c.crawl()
