6f259912418a4359e993d114ca4378d9599d31e3
coid <xiaojue1990@gmail.com>
2014-10-11 21:03:05 +0800

    delete some redundancy

--- Client/Logistics/src/com/logistics/activity/MapActivity.java
+++ Client/Logistics/src/com/logistics/activity/MapActivity.java
@@ -1,22 +1,84 @@
 package com.logistics.activity;
 
+import java.util.ArrayList;
+
+import me.maxwin.view.XListView;
+import me.maxwin.view.XListView.IXListViewListener;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
 
 import com.logistics.R;
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ArrayAdapter;
 
 @ContentView(R.layout.activity_map)
-public class MapActivity extends RoboActivity {
+public class MapActivity extends RoboActivity  implements IXListViewListener{
 
 	public static final String TAG = MapActivity.class.getSimpleName();
 	
+	private XListView mListView;
+	private ArrayAdapter<String> mAdapter;
+	private ArrayList<String> items = new ArrayList<String>();
+	private Handler mHandler;
+	private int start = 0;
+	private static int refreshCnt = 0;
+	/** Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
+		geneItems();
+		mListView = (XListView) findViewById(R.id.xListView);
+		mListView.setPullLoadEnable(true);
+		mAdapter = new ArrayAdapter<String>(this, R.layout.list_item, items);
+		mListView.setAdapter(mAdapter);
+//		mListView.setPullLoadEnable(false);
+//		mListView.setPullRefreshEnable(false);
+		mListView.setXListViewListener(this);
+		mHandler = new Handler();
 	}
 
+	private void geneItems() {
+		for (int i = 0; i != 20; ++i) {
+			items.add("refresh cnt " + (++start));
+		}
+	}
+
+	private void onLoad() {
+		mListView.stopRefresh();
+		mListView.stopLoadMore();
+		mListView.setRefreshTime("刚刚");
+	}
+	
+	@Override
+	public void onRefresh() {
+		mHandler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				start = ++refreshCnt;
+				items.clear();
+				geneItems();
+				// mAdapter.notifyDataSetChanged();
+				mAdapter = new ArrayAdapter<String>(MapActivity.this, R.layout.list_item, items);
+				mListView.setAdapter(mAdapter);
+				onLoad();
+			}
+		}, 2000);
+	}
+
+	@Override
+	public void onLoadMore() {
+		mHandler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				geneItems();
+				mAdapter.notifyDataSetChanged();
+				onLoad();
+			}
+		}, 2000);
+	}
 	
 
 }
