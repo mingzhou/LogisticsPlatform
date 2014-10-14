c6ee6f40d581a6646ba6bd8af8191f2e1ab8a1c9
Brilliant von Shakespeare <brilliantshakespeare@gmail.com>
2014-10-14 17:13:25 +0800

    abstract the mongodb module

--- Server/crawlers/mongodb.py
+++ Server/crawlers/mongodb.py
@@ -1,17 +1,1 @@
-from pymongo import MongoClient
-
-class MongoDB():
-    DB_HOST = "spider"
-    DATABASE = "demo"
-    COLLECTION = "supply"
-    
-    def __init__(self):
-        client = MongoClient(self.DB_HOST)
-        database = client[self.DATABASE]
-        self.collection = database[self.COLLECTION]
-
-    def insert(self, data):
-        self.collection.insert(data) 
-
-    def find(self, obj):
-        return self.collection.find(obj).count()
+../mongo/mongodb.py
