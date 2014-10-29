503eec704efda9c3841ec7a146ff2aeae354e5af
Brilliant von Shakespeare <brilliantshakespeare@gmail.com>
2014-10-29 16:13:52 +0800

    add query to tornado

--- Server/tornado/hello.py
+++ Server/tornado/hello.py
@@ -33,11 +33,21 @@ class PreviousHandler(tornado.web.RequestHandler):
         self.write(database.previous(dict_id))
         self.flush()
 
+class QueryHandler(tornado.web.RequestHandler):
+    def post(self):
+        str_json = self.get_argument("data")
+        dict_json = eval(str_json)
+        for key in dict_json:
+            dict_json[key] = {"$regex": dict_json[key]}
+        self.write(database.query(dict_json))
+        self.flush()
+
 
 tornado.options.parse_command_line()
 app = tornado.web.Application(handlers = 
         [(r"/latest", LatestHandler), 
-        (r"/previous", PreviousHandler)])
+        (r"/previous", PreviousHandler),
+        (r"/query", QueryHandler)])
 http_server = tornado.httpserver.HTTPServer(app)
 http_server.listen(options.port)
 tornado.ioloop.IOLoop.instance().start()
