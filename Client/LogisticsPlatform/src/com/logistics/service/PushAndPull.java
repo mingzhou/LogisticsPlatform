cda28666aba838110c1f7a0f99b7441a19230a9a
coid <xiaojue1990@gmail.com>
2015-01-09 19:37:36 +0800

    小红点振动提示bug

--- Client/LogisticsPlatform/src/com/logistics/service/PushAndPull.java
+++ Client/LogisticsPlatform/src/com/logistics/service/PushAndPull.java
@@ -189,8 +189,9 @@ public class PushAndPull extends Service {
 				}
 			}else if(msg.what!=0&& update_size > 0 &&msg.arg1!=0 ){
 				Intent intent = new Intent("com.example.communication.RECEIVER"); 
+				if(isAppOnForeground()){
 				intent.putExtra("data",update_size);
-				intent.putExtra("background", false);
+				intent.putExtra("background", false);}
 				sendBroadcast(intent);
 			}
 			
